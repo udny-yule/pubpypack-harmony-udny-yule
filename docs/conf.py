54a08ddd58a850b1f13406df93c94fccf56ff3f4
udny-yule <17485220+udny-yule@users.noreply.github.com>
2023-02-03 00:35:46 -0500

    Add apidoc integration for builds on Read the Docs

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,10 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sphinx
+from importlib import metadata
+
 
 project = 'pubpypack-harmony-udny-yule'
 copyright = '2023, Udny Yule'
@@ -32,7 +36,29 @@ html_static_path = ['_static']
 
 
 
-from importlib import metadata
-
 PACKAGE_VERSION = metadata.version('pubpypack-harmony-udny-yule')
 version = release = PACKAGE_VERSION
+
+
+if os.environ.get("READTHEDOCS") == "True":
+    from pathlib import Path
+
+    PROJECT_ROOT = Path(__file__).parent.parent
+    PACKAGE_ROOT = PROJECT_ROOT / "src" / "imppkg"
+
+    def run_apidoc(_):
+        from sphinx.ext import apidoc
+        apidoc.main([
+            "--force",
+            "--implicit-namespaces",
+            "--module-first",
+            "--separate",
+            "-o",
+            str(PROJECT_ROOT / "docs" / "reference"),
+            str(PACKAGE_ROOT),
+            str(PACKAGE_ROOT / "*.c"),
+            str(PACKAGE_ROOT / "*.so"),
+        ])
+
+    def setup(app):
+        app.connect('builder-inited', run_apidoc)
